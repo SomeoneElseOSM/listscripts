7169d0bc5c2ab91d6a672cdbe261bfc22b3b57d7
Andy Townsend <ajtown@map.atownsend.org.uk>
2020-05-28 14:26:13 +0200

    Checking in somewhere public

---
+++ listpeople.sh
@@ -0,0 +1,10 @@
+# ----------------------------------------------------------------------------
+# listpeople.sh $1 $2 $3
+# Who has posted to OSM mailing list $1 in year $2 month $3 ?
+# Requires lynx (commad line browser)
+#
+# Example usage:
+# listpeople.sh tagging 2020 May
+# ----------------------------------------------------------------------------
+lynx -width=1024 --dump "https://lists.openstreetmap.org/pipermail/${1}/${2}-${3}/date.html#start" | grep -i "\[.*${1}\]" | sed "s/.*  //" | sort | uniq -c | sort -n -r | head -20
+#
